fbf75d6588125489278befa4d8b70cd7e91ca5b2
Henry Miller <mail.henrym+git@gmail.com>
2022-01-01 22:07:16 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names,event) {
+    const msgs = [];
+    for (let i = 0; i < names.length; i++){
+        msgs.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return msgs;
+    
+}
+
+function countDown(num) {
+    let numx = num;
+    while (numx !== -1) {
+        console.log(numx);
+        numx -=1;
+    }
+}
